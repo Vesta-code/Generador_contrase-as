b9f76a98c29014e3deb9f9ded19348edc5f86df9
Erika Rheineck <rheinat3@gmail.com>
2024-09-18 07:09:47 -0400

    subir archivos base

---
+++ main.js
@@ -0,0 +1,39 @@
+let cantidad = document.getElementById('cantidad');
+let boton = document.getElementById('generar');
+let contrasena = document.getElementById('contrasena')
+
+const cadenaCaracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@uSX^&*()";
+
+function generar(){
+
+    let numeroDigitado = parseInt (cantidad.value);
+
+    if( numeroDigitado < 8){
+        alert("la cantidad de caracteres tiene que ser mayor que 8");
+    }
+
+    let password = '';
+    for(let i=0; i < numeroDigitado; i++){
+
+        let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)];
+        console.log(caracterAleatorio);
+
+        password+=caracterAleatorio;
+
+
+    }
+
+    contrasena.value = password;
+
+}       
+
+
+
+
+
+
+
+
+
+
+
